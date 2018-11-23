c3b360455dee5a75456fcbe3aec51b2fbcc4f9dd
eeyler <43970642+eeyler@users.noreply.github.com>
2018-11-23 15:40:05 +0000

    users.sql
    users table with 3 records

--- database/users.sql
+++ database/users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 19, 2018 at 08:46 PM
--- Server version: 5.7.21
--- PHP Version: 5.6.35
+-- Generation Time: Nov 23, 2018 at 03:38 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -43,8 +43,18 @@ CREATE TABLE IF NOT EXISTS `users` (
   `postcode` text NOT NULL,
   `level` enum('admin','member') NOT NULL,
   `join_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`user_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `email`, `password`, `first_name`, `last_name`, `dob`, `phone`, `house_no`, `street`, `city`, `postcode`, `level`, `join_date`) VALUES
+(1, 'admin@biomarket.co.uk', '$2y$10$nm9heArrNVNrm4QTPKBfFeqoHmi.8zaxCeRsF/6PnXSON4fDMpGz.', 'Admin', 'BioMarket', '2018-10-06', '20 8231 2468', 'University of West London', 'St. Mary Road, Eailing', 'London', 'W5 5RF', 'admin', '2018-11-23 14:18:56'),
+(2, 'edit.egri@yahoo.co.uk', '$2y$10$NeUoE2ta0ivz6oQacS2touhlsF/T3qImYddEuaiS5D0uKoS8CI3Jq', 'Edit', 'Egri', '2018-11-03', '123456789', '44', 'Staines Road', 'London', 'W5 5RF', 'member', '2018-11-23 14:21:22'),
+(3, 'eyler@freemail.hu', '$2y$10$dfYIBMNUJRNPNzOdWia2muXfHt9uyZ3ktfLzd1lk5y.xHSTpmMEWi', 'James', 'Brown', '2018-11-05', '20 8231 2468', '111', 'High', 'London', 'Uhdf5 6hf', 'member', '2018-11-23 14:24:35');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
