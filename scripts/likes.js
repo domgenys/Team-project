dc67e2570ed1cdc0cda3c4a204499fc150dfda80
GeorgeAlexandruS <>
2018-11-27 19:18:43 +0000

    tables and product card style

--- scripts/likes.js
+++ scripts/likes.js
@@ -1,7 +1,4 @@
-/* 
- * Love button for Design it & Code it
- * http://designitcodeit.com/i/9
- */
+
 $('.btn-counter').on('click', function(event, count) {
   event.preventDefault();
   
@@ -10,17 +7,6 @@ $('.btn-counter').on('click', function(event, count) {
       active = $this.hasClass('active'),
       multiple = $this.hasClass('multiple-count');
   
-  // First method, allows to add custom function
-  // Use when you want to do an ajax request
-  /* if (multiple) {
-  $this.attr('data-count', ++count);
-  // Your code here
-  } else {
-  $this.attr('data-count', active ? --count : ++count).toggleClass('active');
-  // Your code here
-  } */
-  
-  // Second method, use when ... I dunno when but it looks cool and that's why it is here
   $.fn.noop = $.noop;
   $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('active');
   
